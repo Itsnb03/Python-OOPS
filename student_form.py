fe306e5076022aaf15145984181c826d5cee05f8
Nitin Bharadwaj <nitinbharadwaj47@gmail.com>
2025-01-25 12:47:56 +0530

    Add fullname method and demonstrate multiple objects

--- student_form.py
+++ student_form.py
@@ -6,8 +6,13 @@ class Student:
         self.course = course
         self.div = div
         self.address=address
+        
+    def full_detail(self):
+        return f"{self.rollno} {self.firstname} {self.lastname} {self.course} {self.div} {self.address} "
+    
 
 std1 = Student("1", "Nitin", "Bharadwaj", "BSCIT", "A", "Borivali")
+print(std1.full_detail())
 print(std1.rollno)
 print(std1.firstname)
 print(std1.lastname)
@@ -16,6 +21,7 @@ print(std1.div)
 print(std1.address)
 
 std2 = Student("2", "Aman", "Singh", "BCSIT", "A", "Kandivali")
+print(std2.full_detail())
 print(std2.rollno)
 print(std2.firstname)
 print(std2.lastname)
