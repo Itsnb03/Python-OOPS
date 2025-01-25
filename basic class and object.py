3e7d3c1a1cd639b988c1852ce9fb9f5128be5973
Nitin Bharadwaj <nitinbharadwaj47@gmail.com>
2025-01-25 12:37:42 +0530

    feat: Add fullname method to Car class
    - Defined a fullname method in the Car class to return a formatted string combining brand and model.
    - Demonstrated functionality with a sample Car object (my_car).

--- basic class and object.py
+++ basic class and object.py
@@ -2,11 +2,17 @@ class Car: #created class
     def __init__(self, brand, model): #parameters passed by user when created object
         self.brand = brand #class variables
         self.model = model #class variables
+        
+    def fullname(self):
+        return f"{self.brand} {self.model}"
 
 my_car = Car("Honda","City") #object 
 print(my_car.brand) #print function for model
 print(my_car.model) #print function for model
+print(my_car.fullname())
 
 my_new_car = Car("Mahindra","XUV")
+
 print(my_new_car.brand)
 print(my_new_car.model) 
+print(my_new_car.fullname())
