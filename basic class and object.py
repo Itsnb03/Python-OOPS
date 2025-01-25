1e4b9d64036928318a20085a7fea57240fbd178f
Nitin Bharadwaj <nitinbharadwaj47@gmail.com>
2025-01-25 12:03:09 +0530

    feat: Add Car class with brand and model attributes
    - Implemented the Car class with an __init__ method to initialize brand and model attributes.
    - Created a sample object (my_car) and printed the brand and model values to demonstrate usage.

--- basic class and object.py
+++ basic class and object.py
@@ -1,6 +1,8 @@
-class Car:
-    brand=None
-    model=None
+class Car: #created class
+    def __init__(self, brand, model): #parameters passed by user when created object
+        self.brand = brand #class variables
+        self.model = model #class variables
 
-my_car= Car()
-print(my_car.brand)
+my_car = Car("Honda","City") #object 
+print(my_car.brand) #print function for model
+print(my_car.model) #print function for model
