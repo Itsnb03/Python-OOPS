48f9ea4409f5bfa55333d348465a114e742a7904
Nitin Bharadwaj <nitinbharadwaj47@gmail.com>
2025-01-25 13:08:33 +0530

    feat: Add ElectricCar subclass with battery_size attribute
    - Extended the Car class by creating an ElectricCar subclass.
    - Added a battery_size attribute to the ElectricCar class and initialized it using the __init__ method.
    - Demonstrated functionality by creating a Tesla object (ElectricCar) and printing its brand, model, and battery size.
    - Retained examples for the Car class (my_car and my_new_car) to ensure compatibility.

--- basic class and object.py
+++ basic class and object.py
@@ -1,18 +1,31 @@
-class Car: #created class
-    def __init__(self, brand, model): #parameters passed by user when created object
-        self.brand = brand #class variables
-        self.model = model #class variables
-        
-    def fullname(self):
-        return f"{self.brand} {self.model}"
+    class Car: #created class
+        def __init__(self, brand, model): #parameters passed by user when created object
+            self.brand = brand #class variables
+            self.model = model #class variables
+            
+        def fullname(self):
+            return f"{self.brand} {self.model}"
 
-my_car = Car("Honda","City") #object 
-print(my_car.brand) #print function for model
-print(my_car.model) #print function for model
-print(my_car.fullname())
+    class ElectricCar(Car):
+        def __init__(self, brand, model, battery_size):
+            super().__init__(brand,model)
+            self.battery_size = battery_size
+            
+            
+    tesla = ElectricCar("Tesla","Model S", "85kWh")
+    print(tesla.brand)
+    print(tesla.model)
+    print(tesla.battery_size)
+            
 
-my_new_car = Car("Mahindra","XUV")
+    my_car = Car("Honda","City") #object 
+    print(my_car.brand) #print function for model
+    print(my_car.model) #print function for model
+    print(my_car.fullname())
 
-print(my_new_car.brand)
-print(my_new_car.model) 
-print(my_new_car.fullname())
+
+    my_new_car = Car("Mahindra","XUV")
+
+    print(my_new_car.brand)
+    print(my_new_car.model) 
+    print(my_new_car.fullname())
