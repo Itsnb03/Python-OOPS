3f6b1e6bc401298342532407ac952e81b63bfe14
Nitin Bharadwaj <nitinbharadwaj47@gmail.com>
2025-01-25 12:31:58 +0530

    feat: Add Student class with multiple attributes
    - Defined the Student class with attributes: rollno, name, course, div, and address.
    - Created two Student objects (std1 and std2) with sample data.
    - Printed the attributes of each object to demonstrate functionality.

--- student_form.py
+++ student_form.py
@@ -1,21 +1,24 @@
 class Student:
-    def __init__(self, rollno, name,course, div,address):
+    def __init__(self, rollno, firstname, lastname, course, div,address):
         self.rollno = rollno
-        self.name = name
+        self.firstname = firstname
+        self.lastname = lastname
         self.course = course
         self.div = div
         self.address=address
 
-std1 = Student("4", "Nitin", "BSCIT", "A", "Borivali")
+std1 = Student("1", "Nitin", "Bharadwaj", "BSCIT", "A", "Borivali")
 print(std1.rollno)
-print(std1.name)
+print(std1.firstname)
+print(std1.lastname)
 print(std1.course)
 print(std1.div)
 print(std1.address)
 
-std2 = Student("5", "Aman", "BCSIT", "A", "Kandivali")
+std2 = Student("2", "Aman", "Singh", "BCSIT", "A", "Kandivali")
 print(std2.rollno)
-print(std2.name)
+print(std2.firstname)
+print(std2.lastname)
 print(std2.course)
 print(std2.div)
 print(std2.address)
